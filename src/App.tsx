c6116f5bcff33e6ca9d6c61987cf12a5e1d039af
Yongjin An <yongjin5352@gmail.com>
2023-08-01 15:15:27 +0900

    feat: 초기 설정 완료
    라우터 구조 초기 설정과 폴더 구조 기본 설정을 완료하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import PageLayout from './layout/PageLayout';
+import MainPage from './pages/MainPage';
+import MusicDetailPage from './pages/MusicDetailPage';
+import UserPage from './pages/UserPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<PageLayout />}>
+        <Route path='' element={<MainPage />} />
+        <Route path='musics/:musicId' element={<MusicDetailPage />} />
+        <Route path='users/:userId' element={<UserPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/MusicDetailPage.tsx
@@ -0,0 +1,5 @@
+function MusicDetailPage() {
+  return <div>MusicDetailPage</div>;
+}
+
+export default MusicDetailPage;
