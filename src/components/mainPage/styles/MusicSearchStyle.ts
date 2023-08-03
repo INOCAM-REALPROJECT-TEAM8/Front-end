8cb033109f35b76992fbae02d1322488abf35f75
Yongjin An <yongjin5352@gmail.com>
2023-08-03 13:53:29 +0900

    feat: 메인페이지 배너 완성
    메인페이지 배너를 완성하였습니다.
    우선 3초마다 자동으로 넘어가도록 설정하였습니다.

--- src/components/mainPage/styles/MusicSearchStyle.ts
+++ src/components/mainPage/styles/MusicSearchStyle.ts
@@ -4,7 +4,6 @@ export const MusicSearchContainer = styled.div`
   position: relative;
   height: 96px;
   width: 100%;
-  max-width: 600px;
   padding: 10px 24px 11px;
   background-color: var(--main-color);
 `;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
+import Banner from '../components/mainPage/Banner';
 import MusicSearch from '../components/mainPage/MusicSearch';
 
 function MainPage() {
   return (
     <>
       <MusicSearch />
+      <Banner />
     </>
   );
 }
