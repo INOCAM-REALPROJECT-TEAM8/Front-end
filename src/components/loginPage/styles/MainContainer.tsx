7fbdeb41ab2b522d85f1a400509827b91768ea43
jihyunjoo2023 <smallpig10@naver.com>
2023-08-04 01:39:12 +0900

    [refactor] nickname change add
    닉네임 수정 기능 추가

---
+++ src/components/loginPage/styles/MainContainer.tsx
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: var(--main-color);
+  padding-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default MainContainer;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,53 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
-<<<<<<< HEAD
 import kakaoimage from '../images/kakao_login_medium_wide.png';
-=======
-import GoogleLogin from './GoogleLogin';
->>>>>>> origin/feature/music-detail-page
+import GoogleLogin from '../features/GoogleLogin';
+import MainContainer from '../components/loginPage/styles/MainContainer';
 
-const LoginPage: React.FC = () => {
+function LoginPage() {
   return (
-    <LoginContainer>
+    <MainContainer>
       <TextPage>HIDE</TextPage>
       <LoginBox>
         <Input type='text' placeholder='Email' />
         <Input type='password' placeholder='Password' />
         <Button>로그인</Button>
         <Button>회원가입</Button>
-<<<<<<< HEAD
-        <SocialButton>구글 로그인</SocialButton>
-        <a href='http://13.125.205.172:8080/login/oauth2/kakao'>
-          <img src={kakaoimage} alt='카카오 로그인'></img>
-        </a>
-=======
         <SocialButton>
           <GoogleLogin />
         </SocialButton>
-
-        <SocialButton>카카오 로그인</SocialButton>
->>>>>>> origin/feature/music-detail-page
+        <a href='http://13.125.205.172:8080/login/oauth2/kakao'>
+          <img src={kakaoimage} alt='카카오 로그인'></img>
+        </a>
       </LoginBox>
-    </LoginContainer>
+    </MainContainer>
   );
-};
+}
 
 export default LoginPage;
 
-const LoginContainer = styled.div`
-  width: 390px;
-  height: 732px;
-  background-color: #7751e1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
+// const LoginContainer = styled.div`
+//   width: 390px;
+//   height: 732px;
+//   background-color: #7751e1;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// `;
 
 const TextPage = styled.div`
   color: white;
   font-size: xx-large;
   margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
 `;
 
 const LoginBox = styled.div`
@@ -66,7 +60,7 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-  width: 250px;
+  width: 300px;
   height: 40px;
   margin-bottom: 10px;
   background-color: #ffffff;
@@ -76,6 +70,6 @@ const Button = styled.button`
 `;
 
 const SocialButton = styled(Button)`
-  width: 250px;
+  width: 300px;
   height: 40px;
 `;
