feed8b255b46c637d6e19cb774ef2a1ab7678034
Yongjin An <yongjin5352@gmail.com>
2023-08-09 15:06:13 +0900

    feat: 회원가입 페이지 뼈대

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,7 +1,19 @@
+import useValidateInput from '../hooks/useValidateInput';
+
 function SignUpPage() {
-  return (<div>
-    
-  </div>);
+  const { input: email, handleInputOnChange: handleEmailOnChange, valid: emailValid } = useValidateInput('email');
+  const { input: password, handleInputOnChange: handlePwOnChange, valid: passwordValid } = useValidateInput('password');
+  const { input: password2, handleInputOnChange: handlePw2OnChange } = useValidateInput();
+  const { input: nickname, handleInputOnChange: handleNicknameOnChange, valid: nicknameValid } = useValidateInput();
+
+  return (
+    <div>
+      <input onChange={handleEmailOnChange} value={email} />
+      <input onChange={handlePwOnChange} value={password} />
+      <input onChange={handlePw2OnChange} value={password2} />
+      <input onChange={handleNicknameOnChange} value={nickname} />
+    </div>
+  );
 }
 
 export default SignUpPage;
